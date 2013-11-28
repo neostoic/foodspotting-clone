c50729db4970f5affc2c04af68d891cc888fe129
viabledish <viable.dish@gmail.com>
2013-11-28 22:01:42 +0000

    fix silly typo

--- app/uploaders/dish_uploader.rb
+++ app/uploaders/dish_uploader.rb
@@ -13,11 +13,11 @@ class DishUploader < CarrierWave::Uploader::Base
     CarrierWave.configure do |config|
       config.fog_credentials = {
         :provider               => 'AWS',
-        :aws_access_key_id      => ENV[:AWS_ACCESS_KEY_ID] 
-        :aws_secret_access_key  => ENV[:AWS_SECRET_ACCESS_KEY]
+        :aws_access_key_id      => ENV[:AWS_ACCESS_KEY_ID],
+        :aws_secret_access_key  => ENV[:AWS_SECRET_ACCESS_KEY],
         :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
       }
-      config.fog_directory  = ENV[:S3_BUCKET] 'foodspotting-clone'                     # required
+      config.fog_directory  = ENV[:S3_BUCKET]
       #config.fog_public     = false                                   # optional, defaults to true
       #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
     end
